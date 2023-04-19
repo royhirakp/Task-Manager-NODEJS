2b8c181f273077018a35f792c33d2a6a6c33c66c
hirak-gitv2 <royhirakp@gmail.com>
2023-04-19 17:55:44 +0530

    comit 3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -38,30 +38,67 @@ route.post('/singUp', async(req,res)=>{
     }    
 })
 
+
 route.post('/login', async (req,res)=>{
     try {
         const {email , password} = req.body;        
-        let user = await userModel.aggregate([{$match:{email:email}}])
-        if(!user.length){
-            return res.status(400).json({status:"user doesnot exist. regster please"})
-        }
-        bcrypt.compare(password, user[0].password, (err,result)=>{
-            if(err){
-                return res.status(500).json({messege: err.message})
-            }
-            if(result){
-                const token = jwt.sign({data:user[0]._id},process.env.jwtSerectKey,{expiresIn:"5h"} )
-                res.json({
-                    email,
-                    status: "login Sucessfull",
-                    token
-                })
-            }
-        })               
+        let userdata = await userModel.findOne({ email })
+        console.log(userdata)
+        if (userdata) {
+            
+           let result = await bcrypt.compare(password, userdata.password);
+           console.log('BIRICPT RESULT', result)
+           if(result){
+            //token
+            const token = jwt.sign({
+                exp: Math.floor(Date.now()/1000)+ 60*60,
+                data : userdata._id,
+            },process.env.jwtSerectKey);
+            console.log('TOKEN GENARATED ', token)
+            res.json({
+                status:"sucesess",
+                token
+            })
+           }else{
+            res.status(400).json({
+                status:'password not matched',
+            })
+           }
+        }else{
+            res.status(400).json({
+                status:'user not register/ register brfore login',
+            })
+        }             
     } catch (error) {
         return res.status(500).json({errr: error.messege})
     }
 })
+
+
+// route.post('/login', async (req,res)=>{
+//     try {
+//         const {email , password} = req.body;        
+//         let user = await userModel.find({email})
+//         if(!user){
+//             return res.status(400).json({status:"user doesnot exist. regster please"})
+//         }
+//         bcrypt.compare(password, user.password, (err,result)=>{
+//             if(err){
+//                 return res.status(500).json({messege: err.message})
+//             }
+//             if(result){
+//                 const token = jwt.sign({data:user._id},process.env.jwtSerectKey,{expiresIn:"5h"} )
+//                 res.json({
+//                     email,
+//                     status: "login Sucessfull",
+//                     token
+//                 })
+//             }
+//         })               
+//     } catch (error) {
+//         return res.status(500).json({errr: error.messege})
+//     }
+// })
 
 
 
